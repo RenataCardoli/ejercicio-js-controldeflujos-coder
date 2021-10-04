b5edaadb33bc81c634db38f5807f90a93fc468c6
Renata Cardoso <renata.cardoli@gmail.com>
2021-10-04 13:37:49 -0300

    Correcion del (if else) para que cumpla la comparacion de los valores de numero1 y numero2 correctamente

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let numero1 = parseInt(prompt("ingrese un numero:",0));
 let numero2 = parseInt(prompt("ingrese un nuevo numero:",0));
 
-if((numero1 >= 1) && (numero2 <= 10)){
+if(numero1 >= 1 && numero1 <= 10 && numero2>=1 && numero2 <=10) {
     alert("Los números ingresados estan entre 1 y 10 y son: "+numero1+" y "+numero2);
 }else{
-    alert("Uno de los numeros ingresados no estan entre 1 y 10 y son: "+numero1+" y "+numero2);
+    alert("Uno de los numeros ingresados no esta entre 1 y 10 y son: "+numero1+" y "+numero2);
 }
